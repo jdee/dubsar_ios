054b56645155538614730043dc60e71a01a02fac
Jimmy Dee <jimmy@dee.name>
2011-08-04 18:39:59 -0500

    better icons

--- Dubsar/DubsarNavigationController_iPhone.m
+++ Dubsar/DubsarNavigationController_iPhone.m
@@ -73,7 +73,7 @@
 
 - (void)addBackButton
 {
-    UIImage* backButtonImage = [UIImage imageNamed:@"wedge-blue-l-narrow.png"];
+    UIImage* backButtonImage = [UIImage imageNamed:@"wedge-blue-l.png"];
     UIBarButtonItem* backButtonItem = [[[UIBarButtonItem alloc]initWithImage:backButtonImage style:UIBarButtonItemStylePlain target:self action:@selector(back)]autorelease];
     
     self.topViewController.navigationItem.leftBarButtonItem = backButtonItem;
@@ -82,7 +82,7 @@
 
 - (void)addForwardButton
 {
-    UIImage* forwardButtonImage = [UIImage imageNamed:@"wedge-blue-r-narrow.png"];
+    UIImage* forwardButtonImage = [UIImage imageNamed:@"wedge-blue-r.png"];
     UIBarButtonItem* forwardButtonItem = [[[UIBarButtonItem alloc]initWithImage:forwardButtonImage style:UIBarButtonItemStylePlain target:self action:@selector(forward)]autorelease];
                                            
     self.topViewController.navigationItem.rightBarButtonItem = forwardButtonItem;
