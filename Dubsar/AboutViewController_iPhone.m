87b9685c3d15be23b57a975094e396a3ef518cda
Jimmy Dee <jimmy@dee.name>
2013-02-14 12:13:07 -0800

    #64: Don't forget UA copyright.

--- Dubsar/AboutViewController_iPhone.m
+++ Dubsar/AboutViewController_iPhone.m
@@ -174,7 +174,7 @@
 {
     return @"<!DOCTYPE html><html><body style='color:#f85400; background-color:#000; font: bold 24pt Trebuchet MS'>"
     "<p>Dubsar is free, open-source software distributed under version 2 of the GNU General Public License.</p><hr/>"
-    "<h2>Dubsar Dictionary Project</h2><h3>Copyright &copy; 2010-2013 Jimmy Dee</h3>"
+    "<h2>Dubsar Dictionary Project</h2><h3>Copyright &copy; 2010&ndash;2013 Jimmy Dee</h3>"
     "<p>This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.</p>"
     "<p>This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.</p>"
     "<p>You should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.</p><hr/>"
@@ -182,6 +182,7 @@
     "<p>WordNet&reg; is available under the WordNet 3.0 License.<hr/>"
     "<p>JSONKit &copy; 2011 John Engelhart</p>"
     "<p>JSONKit is dual licensed under either the terms of the BSD License, or alternatively under the terms of the Apache License, Version 2.0.</p><hr/>"
+    "<p>Urban Airship iOS Library &copy; 2009&ndash;2013 Urban Airship Inc.</p>"
     "<p>THIS SOFTWARE IS PROVIDED BY THE URBAN AIRSHIP INC ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL URBAN AIRSHIP INC OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.</p>"
     "</body></html>";
 }
