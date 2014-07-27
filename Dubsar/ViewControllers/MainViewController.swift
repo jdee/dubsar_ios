8c4de4260f3667703f6132bc4e89592877f7c788
Jimmy Dee <jgvdthree@gmail.com>
2014-07-27 10:02:34 -0700

    Much improved animation. Probably not the last word.

--- Dubsar/ViewControllers/MainViewController.swift
+++ Dubsar/ViewControllers/MainViewController.swift
@@ -78,6 +78,8 @@ class MainViewController: BaseViewController, UIAlertViewDelegate, UISearchBarDe
         /* avoid the default behavior
         super.didRotateFromInterfaceOrientation(fromInterfaceOrientation) // calls adjustLayout()
         */
+
+        // NSLog("Actual view size: %f x %f", Double(view.bounds.size.width), Double(view.bounds.size.height))
     }
 
     override func willRotateToInterfaceOrientation(toInterfaceOrientation: UIInterfaceOrientation, duration: NSTimeInterval) {
@@ -89,51 +91,104 @@ class MainViewController: BaseViewController, UIAlertViewDelegate, UISearchBarDe
 
         if alphabetView {
             // DEBT: Move this stuff into the AlphabetView
-            let π = CGFloat(M_PI)
-
-            var transform = CGAffineTransformIdentity
-
             alphabetView.hidden = false
 
-            // position the alphabet view at its new position after rotation
-            // In each case, move the view to its current location in the new view frame, without resizing, so that the lower
-            // righthand corner of the alphabetView is in the lower righthand corner of the view before the rotation begins.
+            // First position the alphabet view where it will be after the rotation, but unrotated.
+            // Move the view without resizing so that the lower righthand corner of the alphabetView
+            // is in the lower righthand corner of the new view frame before the rotation begins.
 
-            var inset: CGFloat = 0
-            let fudge : CGFloat = UIDevice.currentDevice().userInterfaceIdiom == .Phone ? 12 : 0 // the difference in the height of the nav bar between orientations
+            // Getting this right has been a hassle. This works, but can probably be simplified.
+            var newScreenWidth: CGFloat = 0
+            var newScreenHeight: CGFloat = 0
+
+            /*
+             * Somehow, on the iPhone, the main screen size is always measured in portrait, so that height > width.
+             * On the iPad, it's just measured in the current orientation, so height may be less than width.
+             */
+            let rotatingToPortrait = UIInterfaceOrientationIsPortrait(toInterfaceOrientation)
+            let isIPad = UIDevice.currentDevice().userInterfaceIdiom == .Pad
+
+            if !isIPad && rotatingToPortrait {
+                newScreenWidth = UIScreen.mainScreen().bounds.size.width
+                newScreenHeight = UIScreen.mainScreen().bounds.size.height
+            }
+            else {
+                newScreenWidth = UIScreen.mainScreen().bounds.size.height
+                newScreenHeight = UIScreen.mainScreen().bounds.size.width
+            }
+
+            let newViewWidth = newScreenWidth
+            var newViewHeight: CGFloat = newScreenHeight - navigationController.navigationBar.bounds.size.height - 20 // 20 for the status bar
+
+            let fudge: CGFloat = UIDevice.currentDevice().userInterfaceIdiom == .Phone ? 12 : 0 // the difference in the height of the nav bar between orientations
+            newViewHeight += UIInterfaceOrientationIsPortrait(toInterfaceOrientation) ? -fudge : +fudge
+
+            // newViewWidth and newViewHeight are now the correct dimensions of this view after the rotation to come.
+
             //*
-            // Then rotate around that lower righthand corner in each case, rather than the center of the view.
+            alphabetView.frame.origin.x = newViewWidth - alphabetView.bounds.size.width
+            alphabetView.frame.origin.y = newViewHeight - alphabetView.bounds.size.height
+
+            // Now when the rotation occurs and the animation below begins, the alphabet view will have its original size and orientation,
+            // but be lined up with the lower righthand corner of the view after the rotation, at the beginning of the animation.
+
+            /*
+            NSLog("new view size: %f x %f. alphabet view size: %f x %f. alphabet view origin: %f, %f", Double(newViewWidth), Double(newViewHeight),
+                Double(alphabetView.bounds.size.width), Double(alphabetView.bounds.size.height), Double(alphabetView.frame.origin.x), Double(alphabetView.frame.origin.y))
+            // */
+
+            //*
+            /* Now rotate around that lower righthand corner in each case, rather than the center of the view. Actually, rotation
+             * occurs around a stationary point just inside the lower righthand corner at the center of a square
+             * with sides equal to the short side of the alphabet view. One of its sides is the far end of the alphabet view (bottom
+             * or right).
+             *
+             * In each case, aspect is the ratio of the long side (of the alphabet view) in the new orientation to the long side
+             * in the current orientation. The inset variable is half the short side of the alphabet view.
+             * The distance from the center of the view to the stationary point is half the long side of the view minus the
+             * inset variable.
+             * In each case, the transformations occur like this:
+             *
+             * 5. The scale operation in the 4th step left the center where it was after the rotation and transverse translation, half
+             *    the original long dimension (before the scale) away from the bottom right. But now that the scale has changed the size
+             *    of the alphabet view, it no longer lines up with the bottom right. Finally translate it so that it lines up properly.
+             * 4. Scale the view in the long dimension (after rotation, so y when rotating to portrait and x when landscape).
+             * 3. Translate the stationary point back to the lower righthand corner, where it originally was.
+             * 2. Rotate around the the stationary point.
+             * 1. Translate the stationary point so that it is at the rotation center, originally the center of the view. If rotating
+             *    to portrait, translate left. If rotating to landscape, translate up.
+             */
+            let π = CGFloat(M_PI)
+            var transform = CGAffineTransformIdentity
+            var inset: CGFloat = 0
             if UIInterfaceOrientationIsPortrait(toInterfaceOrientation) {
                 // rotating from the bottom of the view to the right side
-                let newWidth = view.bounds.size.height + navigationController.navigationBar.bounds.size.height
-                let newHeight = view.bounds.size.width - navigationController.navigationBar.bounds.size.height - fudge
-
-                alphabetView.frame.origin.x = newWidth - alphabetView.bounds.size.width
-                alphabetView.frame.origin.y = newHeight - alphabetView.bounds.size.height
-
-                let aspect = (newHeight - searchBar.bounds.size.height) / alphabetView.bounds.size.width
+                let newAlphabetHeight = newViewHeight - searchBar.bounds.size.height
+                let aspect = newAlphabetHeight / alphabetView.bounds.size.width
                 inset = 0.5 * alphabetView.bounds.size.height
-                transform = CGAffineTransformTranslate(transform, 0.5*aspect*alphabetView.bounds.size.width - inset, 0.0)
+                let offset = 0.5 * alphabetView.bounds.size.width - inset
+
+                transform = CGAffineTransformTranslate(transform, 0, 0.5 * (alphabetView.bounds.size.width - newAlphabetHeight))
+                transform = CGAffineTransformScale(transform, 1.0, aspect)
+                transform = CGAffineTransformTranslate(transform, offset, 0.0)
                 transform = CGAffineTransformRotate(transform, 0.5 * π)
-                transform = CGAffineTransformTranslate(transform, -0.5*aspect*alphabetView.bounds.size.width + inset, 0.0)
-                transform = CGAffineTransformScale(transform, aspect, 1.0)
+                transform = CGAffineTransformTranslate(transform, -offset, 0.0)
             }
             else {
                 // rotating from the right side of the view to the bottom
-                let newWidth = view.bounds.size.height + navigationController.navigationBar.bounds.size.width
-                let newHeight = view.bounds.size.width - navigationController.navigationBar.bounds.size.height + fudge
-
-                alphabetView.frame.origin.x = newWidth - alphabetView.bounds.size.width
-                alphabetView.frame.origin.y = newHeight - alphabetView.bounds.size.height
-
-                let aspect = newWidth / alphabetView.bounds.size.height
+                let aspect = newViewWidth / alphabetView.bounds.size.height
                 inset = 0.5 * alphabetView.bounds.size.width
-                transform = CGAffineTransformTranslate(transform, 0.0, 0.5*aspect*alphabetView.bounds.size.height - inset)
-                transform = CGAffineTransformRotate(transform, -0.5 * π)
-                transform = CGAffineTransformTranslate(transform, 0.0, -0.5*aspect*alphabetView.bounds.size.height + inset)
-                transform = CGAffineTransformScale(transform, 1.0, aspect)
-            }
+                let offset = 0.5 * alphabetView.bounds.size.height - inset
 
+                transform = CGAffineTransformTranslate(transform, -0.5 * (newViewWidth - alphabetView.bounds.size.height), 0)
+                transform = CGAffineTransformScale(transform, aspect, 1.0)
+                transform = CGAffineTransformTranslate(transform, 0.0, offset)
+                transform = CGAffineTransformRotate(transform, -0.5 * π)
+                transform = CGAffineTransformTranslate(transform, 0.0, -offset)
+            }
+            // */
+
+            //*
             UIView.animateWithDuration(duration, delay: 0.0, options: .CurveLinear,
                 animations: {
                     [weak self] in
@@ -243,6 +298,8 @@ class MainViewController: BaseViewController, UIAlertViewDelegate, UISearchBarDe
 
         adjustAlphabetView(UIApplication.sharedApplication().statusBarOrientation)
 
+        // NSLog("Actual view size: %f x %f", Double(view.bounds.size.width), Double(view.bounds.size.height))
+
         super.adjustLayout()
     }
 
@@ -319,7 +376,7 @@ class MainViewController: BaseViewController, UIAlertViewDelegate, UISearchBarDe
 
     private func computeAlphabetFrame(orientation: UIInterfaceOrientation) -> CGRect {
         var alphabetFrame = CGRectZero
-        let dimension : CGFloat = 50
+        let dimension: CGFloat = 50 // DEBT <-
 
         if UIInterfaceOrientationIsPortrait(orientation) {
             alphabetFrame = CGRectMake(view.bounds.size.width - dimension, searchBar.bounds.size.height, dimension, view.bounds.size.height - searchBar.bounds.size.height)
@@ -344,7 +401,6 @@ class MainViewController: BaseViewController, UIAlertViewDelegate, UISearchBarDe
 
         alphabetFrame = computeAlphabetFrame(orientation)
 
-        let dimension: CGFloat = 50 // DEBT <-
         alphabetView.transform = CGAffineTransformIdentity
         alphabetView.frame = alphabetFrame
         alphabetView.hidden = false
